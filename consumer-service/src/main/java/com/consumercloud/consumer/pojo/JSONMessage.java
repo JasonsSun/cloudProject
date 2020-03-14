d20888a98294552973a0345af7c835c0aff8ac84
sunyuxing <2849279569@qq.com>
2020-03-15 00:08:59 +0800

    feature:添加用户名获取
    -  #401 添加获取用户名

---
+++ consumer-service/src/main/java/com/consumercloud/consumer/pojo/JSONMessage.java
@@ -0,0 +1,11 @@
+package com.consumercloud.consumer.pojo;
+
+/**
+ * @author 16591
+ * @title: JSONMeaage
+ * @projectName MyGitCloud
+ * @description: TODO
+ * @date 2020/3/14   23:32
+ */
+public class JSONMeaage {
+}
